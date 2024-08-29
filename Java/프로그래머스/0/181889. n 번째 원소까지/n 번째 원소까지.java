1329daeb103d13d096741d33e73d7fc0f85781c0
Boom0704 <126732752+Boom0704@users.noreply.github.com>
2024-08-29 10:01:54 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        int position = 3;
+        int[] arr2 = new int[n];
+        for(int i = 0; i < n; i++)  {
+            arr2[i] = num_list[i];
+         } 
+        return arr2;
+    }
+}
